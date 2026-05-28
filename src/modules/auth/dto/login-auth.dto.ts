7f57000a06643a5ebeb5fabc389807098c28c227
willy <willybalt.2017@gmail.com>
2026-05-28 15:30:43 -0400

    autenticacion con roles

--- src/modules/auth/dto/login-auth.dto.ts
+++ src/modules/auth/dto/login-auth.dto.ts
@@ -1,14 +1,27 @@
-// import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, MaxLength, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    IsEmail,
+    IsNotEmpty,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
 
-export class LoginAuthDto{
+export class LoginAuthDto {
 
-    // @ApiProperty({description: 'Ingrese un correo Valido'})
+    @ApiProperty({
+        example: 'admin@gmail.com',
+        description: 'Correo del usuario',
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
-    // @ApiProperty()
+    @ApiProperty({
+        example: '123456',
+        description: 'Contraseña del usuario',
+        minLength: 6,
+        maxLength: 250,
+    })
     @MinLength(6)
     @MaxLength(250)
     @IsNotEmpty()
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+import {
+    IsEmail,
+    IsEnum,
+    IsString,
+    MinLength,
+} from 'class-validator';
 
 export enum UserRole {
     CUSTOMER = 'CUSTOMER',
@@ -6,16 +13,36 @@ export enum UserRole {
 }
 
 export class CreateUserDto {
+
+    @ApiProperty({
+        example: 'Juan Perez',
+        description: 'Nombre del usuario',
+    })
     @IsString()
     name: string;
 
+    @ApiProperty({
+        example: 'juan@gmail.com',
+        description: 'Correo electrónico',
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        example: '123456',
+        description: 'Contraseña del usuario',
+        minLength: 6,
+    })
     @IsString()
     @MinLength(6)
     password: string;
 
+    @ApiProperty({
+        enum: UserRole,
+        example: UserRole.CUSTOMER,
+        description: 'Rol del usuario',
+        required: false,
+    })
     @IsEnum(UserRole)
-    role?: UserRole; // opcional (por defecto CUSTOMER)
+    role?: UserRole;
 }
