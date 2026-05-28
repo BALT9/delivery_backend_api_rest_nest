34333e6073b943ec5d47c185ef31eb32a1eb947d
willy <willybalt.2017@gmail.com>
2026-05-28 10:04:40 -0400

    feat(users): agregar hash de contraseñas con bcrypt y validación de email único

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -15,21 +17,26 @@ export class AuthService {
   ): Promise<{ access_token: string }> {
 
     try {
-      // console.log('EMAIL:', email);
-      // console.log('PASSWORD:', password);
 
       const user = await this.usersService.findOneByEmail(email);
 
-      console.log('USER:', user);
-
       // Usuario no existe
       if (!user) {
-        throw new UnauthorizedException('Usuario no existe');
+        throw new UnauthorizedException(
+          'Usuario no existe',
+        );
       }
 
-      // Contraseña incorrecta
-      if (user.password !== password) {
-        throw new UnauthorizedException('Contraseña incorrecta');
+      // Comparar password encriptado
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        user.password,
+      );
+
+      if (!isPasswordValid) {
+        throw new UnauthorizedException(
+          'Contraseña incorrecta',
+        );
       }
 
       const payload = {
@@ -38,19 +45,20 @@ export class AuthService {
       };
 
       return {
-        access_token: await this.jwtService.signAsync(payload),
+        access_token: await this.jwtService.signAsync(
+          payload,
+        ),
       };
 
     } catch (error) {
-      // console.error('ERROR LOGIN:', error);
 
-      // Si ya es error de auth, lo devolvemos tal cual
       if (error instanceof UnauthorizedException) {
         throw error;
       }
 
-      // Error genérico
-      throw new UnauthorizedException('Error al hacer login');
+      throw new UnauthorizedException(
+        'Error al hacer login',
+      );
     }
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -14,8 +16,29 @@ export class UsersService {
   ) { }
 
   async create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.save(createUserDto);
-    return user;
+
+    const existingUser = await this.userRepository.findOneBy({
+      email: createUserDto.email
+    })
+
+    if (existingUser) {
+      throw new BadRequestException(
+        'El email ya está registrado'
+      )
+    }
+
+    // Encriptar la contraseña
+    const hashedPassword = await bcrypt.hash(
+      createUserDto.password,
+      10,
+    )
+
+    const user = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword
+    });
+
+    return this.userRepository.save(user);
   }
 
   findAll() {
@@ -47,7 +70,34 @@ export class UsersService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
+
     const user = await this.findOne(id);
+
+    // Validar email repetido
+    if (
+      updateUserDto.email &&
+      updateUserDto.email !== user.email
+    ) {
+
+      const existingUser = await this.userRepository.findOneBy({
+        email: updateUserDto.email,
+      });
+
+      if (existingUser) {
+        throw new BadRequestException(
+          'El email ya está registrado',
+        );
+      }
+    }
+
+    // Encriptar password si viene
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(
+        updateUserDto.password,
+        10,
+      );
+    }
+
     Object.assign(user, updateUserDto);
 
     return this.userRepository.save(user);
