194c372dd78214528e69c09458fd3de4c49e4558
willy <willybalt.2017@gmail.com>
2026-05-31 13:46:21 -0400

    documentacion con swagger

--- src/modules/orders/dto/create-order-item.dto.ts
+++ src/modules/orders/dto/create-order-item.dto.ts
@@ -1,9 +1,20 @@
 import { IsInt, IsUUID, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderItemDto {
+
+  @ApiProperty({
+    example: 'b3c1a2f0-9c2d-4f2e-8b1a-123456789abc',
+    description: 'ID del producto',
+  })
   @IsUUID()
   productId: string;
 
+  @ApiProperty({
+    example: 2,
+    description: 'Cantidad del producto (mínimo 1)',
+    minimum: 1,
+  })
   @IsInt()
   @Min(1)
   quantity: number;
--- src/modules/orders/dto/create-order.dto.ts
+++ src/modules/orders/dto/create-order.dto.ts
@@ -1,8 +1,25 @@
 import { Type } from 'class-transformer';
 import { IsArray, ValidateNested } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 import { CreateOrderItemDto } from './create-order-item.dto';
 
 export class CreateOrderDto {
+
+    @ApiProperty({
+        description: 'Lista de productos del pedido (carrito)',
+        type: [CreateOrderItemDto],
+        example: [
+            {
+                productId: 'b3c1a2f0-9c2d-4f2e-8b1a-123456789abc',
+                quantity: 2,
+            },
+            {
+                productId: 'a91d2c33-7f12-4b11-9f22-987654321def',
+                quantity: 1,
+            },
+        ],
+    })
     @IsArray()
     @ValidateNested({ each: true })
     @Type(() => CreateOrderItemDto)
